8f0ced7d30566a130051e3c3d7c6fb2897af6d36
zlmoment <zlmoment@gmail.com>
2014-05-08 23:40:46 -0500

    return result immediately when found

--- Two Sum/TwoSum-v2.java
+++ Two Sum/TwoSum-v2.java
@@ -20,13 +20,12 @@ public class Solution {
 
         for(int i = 0; i < numbers.length; i++) {
             if(hm.get(target - numbers[i]) != null) {  
-                retVal[0] = hm.get(target - numbers[i]) + 1;  
-                retVal[1] = i + 1;  
+                return new int[] {hm.get(target - numbers[i]) + 1, i + 1};
             } else {  
                 hm.put(numbers[i], i);  
             }  
         }  
 
-        return retVal;  
+        return null;  
     }
 }
