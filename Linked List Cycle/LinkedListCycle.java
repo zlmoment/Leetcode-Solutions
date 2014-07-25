6f6a550af002d864a2c0e3eb2486e94ae7538cd1
Zhaoyu Li <zlmoment@gmail.com>
2014-07-24 23:19:33 -0500

    Add Linked List Cycle

---
+++ Linked List Cycle/LinkedListCycle.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ 
+ /* 
+  * 该问题是经典面试问题，其标准解法是用两个指针，一快一慢，
+  * 如果在快的指针能够追上慢的指针，则有环，否则无环。
+  */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
