da282d99277bb281010f2ae7a21259a65a244a08
Zhaoyu Li <zlmoment@gmail.com>
2014-09-01 20:15:54 -0500

    Add comments to Rotate Image

--- Rotate Image/RotateImage.java
+++ Rotate Image/RotateImage.java
@@ -9,6 +9,13 @@ public class Solution {
                 rtn[j][n-i-1] = matrix[i][j];
             }
         }
+
+        // following loop can NOT write as matrix = rtn;
+        // "matrix" is just a reference to a 2-dimension array. 
+        // If "matrix" is assigned to a new 2-dimension array 
+        // in the method, the original array does not change. 
+        // Therefore, there should be another loop to assign 
+        // each element to the array referenced by "matrix". 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j] = rtn[i][j];
