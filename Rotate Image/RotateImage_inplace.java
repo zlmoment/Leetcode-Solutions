697c6e997a318a48a6d7e3d852f19f96006ab5b2
Zhaoyu Li <zlmoment@gmail.com>
2014-09-01 20:49:25 -0500

    Add Rotate Image - in place

---
+++ Rotate Image/RotateImage_inplace.java
@@ -0,0 +1,18 @@
+// in place solution
+// 交换四个元素，关键是循环的判断条件怎么写
+// reference: http://www.programcreek.com/2013/01/leetcode-rotate-image-java/
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < Math.ceil(((double) n) / 2); j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = temp;
+            }
+        }
+    }
+}
