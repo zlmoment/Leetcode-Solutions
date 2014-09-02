a21b7b10f054bd7cf298197337853dd57bcab739
Zhaoyu Li <zlmoment@gmail.com>
2014-09-01 20:53:56 -0500

    Modify Rotate Image

--- Rotate Image/RotateImage_inplace.java
+++ Rotate Image/RotateImage_inplace.java
@@ -6,7 +6,7 @@ public class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < n / 2; i++) {
-            for (int j = 0; j < Math.ceil(((double) n) / 2); j++) {
+            for (int j = i; j < n - i - 1; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[n-j-1][i];
                 matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
