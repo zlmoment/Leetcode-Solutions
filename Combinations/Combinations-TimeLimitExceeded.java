debbb02a5d268a318d31e9f70a99f61a41451145
Zhaoyu Li <zlmoment@gmail.com>
2014-09-06 22:36:06 -0500

    Add Combinations - Time Limit Exceeded
    DFS

---
+++ Combinations/Combinations-TimeLimitExceeded.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> rtn = new ArrayList<List<Integer>>();
+        
+        int start = 1, num = 0;
+        List<Integer> inList = new ArrayList<Integer>();
+        buildResult(start, num, n, k, inList, rtn);
+        return rtn;
+    }
+    
+    // DFS
+    public static void buildResult(int start, int num, int n, int k, List<Integer> inList, List<List<Integer>> rtn) {
+        if (num == k) {
+            List<Integer> temp = new ArrayList<Integer>(inList);
+            rtn.add(temp);
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            inList.add(i);
+            buildResult(start+1, num+1, n, k, inList, rtn);
+            inList.remove(inList.size() - 1);
+        }
+    }
+}
