40beb209e9a2ff4a27bc4d357c3eedd2a5a21acd
Zhaoyu Li <zlmoment@gmail.com>
2014-09-27 00:31:07 -0500

    Refined Permutations

--- Permutations/Permutations.java
+++ Permutations/Permutations.java
@@ -3,12 +3,12 @@ public class Solution {
         List<List<Integer>> rtn = new ArrayList<List<Integer>>();
         List<Integer> inList = new ArrayList<Integer>();
         
-        perm(rtn, inList, num);
+        dfs(rtn, inList, num);
         
         return rtn;
     }
     
-    private static void perm(List<List<Integer>> rtn, List<Integer> inList, int[] num) {
+    private static void dfs(List<List<Integer>> rtn, List<Integer> inList, int[] num) {
         if (inList.size() == num.length) {
             rtn.add(new ArrayList<Integer>(inList));
             return;
@@ -18,7 +18,7 @@ public class Solution {
                 continue;
             }
             inList.add(num[i]);
-            perm(rtn, inList, num);
+            dfs(rtn, inList, num);
             inList.remove(inList.size() - 1);
         }
     }
