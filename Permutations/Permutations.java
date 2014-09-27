7253f5e9549ae6539adefa35c966a6a56fc639fa
Zhaoyu Li <zlmoment@gmail.com>
2014-09-27 00:29:54 -0500

    Refined Permutations

--- Permutations/Permutations.java
+++ Permutations/Permutations.java
@@ -1,27 +1,25 @@
 public class Solution {
     public List<List<Integer>> permute(int[] num) {
         List<List<Integer>> rtn = new ArrayList<List<Integer>>();
-        List<Integer> tmp = new ArrayList<Integer>();
-        boolean[] visited = new boolean[num.length];
+        List<Integer> inList = new ArrayList<Integer>();
         
-        perm(rtn, tmp, num, visited);
+        perm(rtn, inList, num);
         
         return rtn;
     }
     
-    private static void perm(List<List<Integer>> rtn, List<Integer> tmp, int[] num, boolean[] visited) {
-        if (tmp.size() == num.length) {
-            rtn.add(new ArrayList<Integer>(tmp));
+    private static void perm(List<List<Integer>> rtn, List<Integer> inList, int[] num) {
+        if (inList.size() == num.length) {
+            rtn.add(new ArrayList<Integer>(inList));
             return;
         }
         for (int i = 0; i < num.length; i++) {
-            if (!visited[i]) {
-                tmp.add(num[i]);
-                visited[i] = true;
-                perm(rtn, tmp, num, visited);
-                visited[i] = false;
-                tmp.remove(tmp.size() - 1);
+            if (inList.contains(num[i])) {
+                continue;
             }
+            inList.add(num[i]);
+            perm(rtn, inList, num);
+            inList.remove(inList.size() - 1);
         }
     }
 }
