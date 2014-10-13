01212171e6bfaaac24b4b14f053ec3599dab94e8
Zhaoyu Li <zlmoment@gmail.com>
2014-10-12 22:50:37 -0500

    Add Merge k Sorted Lists - TLE

---
+++ Merge k Sorted Lists/MergeKSortedLists-TLE.java
@@ -0,0 +1,57 @@
+// Time Limitation Exceeded
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(List<ListNode> lists) {
+        if (lists.size() == 0) {
+            return null;
+        }
+        ListNode rtn = lists.get(0);
+        for (int i = 1; i < lists.size(); i++) {
+            rtn = merge(rtn, lists.get(i));
+        }
+        return rtn;
+    }
+    public static ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        ListNode head, pointer;
+        if (l1.val < l2.val) {
+            head = l1;
+            l1 = l1.next;
+        } else {
+            head = l2;
+            l2 = l2.next;
+        }
+        pointer = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                pointer.next = l1;
+                l1 = l1.next;
+            } else {
+                pointer.next = l2;
+                l2 = l2.next;
+            }
+            pointer = pointer.next;
+        }
+        if (l1 != null) {
+            pointer.next = l1;
+        } else {
+            pointer.next = l2;
+        }
+        return head;
+    }
+}
