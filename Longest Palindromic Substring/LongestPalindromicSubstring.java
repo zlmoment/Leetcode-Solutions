cae35cc5d452aac55daa6e3a2073741a0fa6ca07
Zhaoyu Li <zlmoment@gmail.com>
2014-10-19 21:25:35 -0500

    Add Longest Palindromic Substring

---
+++ Longest Palindromic Substring/LongestPalindromicSubstring.java
@@ -0,0 +1,35 @@
+// reference: http://www.programcreek.com/2013/12/leetcode-solution-of-longest-palindromic-substring-java/
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len <= 1) {
+            return s;
+        }
+        String longest = s.substring(0, 1);
+        for (int i = 0; i < len; i++) {
+            // get longest palindrome with center of i
+            String tmp = helper(s, i, i);
+            if (tmp.length() > longest.length()) {
+                longest = tmp;
+            }
+
+            // get longest palindrome with center of i, i+1
+            tmp = helper(s, i, i + 1);
+            if (tmp.length() > longest.length()) {
+                longest = tmp;
+            }
+        }
+        return longest;
+    }
+
+    // Given a center, either one letter or two letter,
+    // Find longest palindrome
+    public static String helper(String s, int begin, int end) {
+        while (begin >= 0 && end <= s.length() - 1 && s.charAt(begin) == s.charAt(end)) {
+            begin--;
+            end++;
+        }
+        return s.substring(begin + 1, end);
+    }
+}
